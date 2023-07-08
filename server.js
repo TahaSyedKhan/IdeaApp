61b82d8a58b4da3a86b9d5b615bea858835fe012
TaHaSyedKhan <exploit.any@gmail.com>
2023-07-08 16:51:41 +0530

    connecting to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express  = require('express');
 const serverConfig = require('./configs/server.config')
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config')
 
 
 const app = express();
+
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", () => {
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", () => {
+    console.log("DB is connected");
+})
 
 
 app.listen(serverConfig.PORT, ()=>{
