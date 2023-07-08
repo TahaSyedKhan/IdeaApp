ef4ecd7057a78b108556e0a4843bc8a94f7cb9a8
TaHaSyedKhan <exploit.any@gmail.com>
2023-07-08 17:30:45 +0530

    created admin user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const serverConfig = require('./configs/server.config')
 const mongoose = require('mongoose');
 const dbConfig = require('./configs/db.config')
+const userModel = require('./models/user.model')
 
 
 const app = express();
@@ -16,8 +17,35 @@ db.on("error", () => {
 
 db.once("open", () => {
     console.log("DB is connected");
-})
 
+    init();
+});
+
+// initialize the mongo db
+// need to create the ADMIN user
+async function init() {
+  
+    let admin = await userModel.findOne({
+        userId: "admin"
+    })
+
+    //Check if admin is already present
+    if (admin) {
+        console.log("Admin Already Exists");
+        return;
+    }
+
+    admin = await userModel.create( {
+        name: "Taha Syed",
+        userId: "admin",
+        email: "tahasyedkhan123@gmail.com",
+        userType: "ADMIN",
+        password: "admin"
+    });
+
+    console.log(admin);
+
+}
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server started on the port number ${serverConfig.PORT}` );
